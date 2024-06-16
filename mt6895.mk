5ee42dc2de1048d6c13843bc67e79762b7829152
danielml <daniel@danielml.dev>
2024-06-16 10:30:40 +0200

    mt6895-common: Build xiaomi-telephony-stub
    To provide the required Xiaomi IMS methods
    
    Change-Id: I1e75476a0d2ed55b5eae2a0a2c6ca4c3c4e2c0f1

--- mt6895.mk
+++ mt6895.mk
@@ -145,6 +145,12 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
 $(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
+PRODUCT_PACKAGES += \
+    xiaomi-telephony-stub
+
+PRODUCT_BOOT_JARS += \
+    xiaomi-telephony-stub
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml \
     $(LOCAL_PATH)/configs/permissions/com.mediatek.ims.config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/com.mediatek.ims.config.xml
