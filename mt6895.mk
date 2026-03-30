35bc8f3e1c30d69000bf95cb7320aa4e276ee7cc
Mashopy <eliasgheeraert@gmail.com>
2026-03-30 03:15:19 +0200

    mt6895-common: Update memtrack target name
    Change-Id: I63777caf54650daa6eecf037ab52affe2f77e8a6

--- mt6895.mk
+++ mt6895.mk
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack-service.mediatek
 
 # DRM
 PRODUCT_PACKAGES += \
