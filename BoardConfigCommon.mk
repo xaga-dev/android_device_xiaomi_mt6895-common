3104b111b51e36536c58a98e522904d01030d7f3
rio004 <riyon336@gmail.com>
2026-08-31 12:26:43 +0100

    mt6895-common: Enable L2CAP and A2DP offload coex mechanism
    Change-Id: I15aeb92166880ed11087bee4287145ce03c96cc7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,6 +139,7 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_BOARD_PLATFORM := mt6895
 
 # Properties
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
