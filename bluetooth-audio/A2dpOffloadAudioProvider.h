614749b46f9a7dd46d65f39bfe161904a66175ad
ChrisCatto <chriscatto@proton.me>
2026-09-09 19:10:40 +0300

    mt6895-common: bluetooth-audio: Implement MediaTek A2DP offload support
    * MediaTek's proprietary implementation configures the DSP via
      AudioALSAHardware during session start, stream state changes, and
      session end. However, it cannot be loaded by AOSP because the
      Bluetooth stack only searches for the standard
      android.hardware.bluetooth.audio interface.
    
    * Implement the required hooks in the standard Bluetooth Audio 2.1
      provider to forward A2DP offload state changes to MediaTek's audio
      HAL.
    
    Change-Id: Ifb77d4ce0efa75b423e02d1885589808e79a4a50
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- bluetooth-audio/A2dpOffloadAudioProvider.h
+++ bluetooth-audio/A2dpOffloadAudioProvider.h
@@ -25,6 +25,12 @@ class A2dpOffloadAudioProvider : public BluetoothAudioProvider {
   Return<void> startSession(const sp<V2_0::IBluetoothAudioPort>& hostIf,
                             const V2_0::AudioConfiguration& audioConfig,
                             startSession_cb _hidl_cb) override;
+  Return<void> startSession_2_1(const sp<V2_0::IBluetoothAudioPort>& hostIf,
+                                const V2_1::AudioConfiguration& audioConfig,
+                                startSession_cb _hidl_cb) override;
+  Return<void> streamStarted(BluetoothAudioStatus status) override;
+  Return<void> streamSuspended(BluetoothAudioStatus status) override;
+  Return<void> endSession() override;
 
  private:
   Return<void> onSessionReady(startSession_cb _hidl_cb) override;
